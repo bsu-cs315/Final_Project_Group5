527a2248a0416eeeacbd2197b9ca0c56aaafc396
caige <caigerogers12@gmail.com>
2022-11-02 15:55:18 -0400

    Added more enviroment space

--- project/Player.gd
+++ project/Player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var _velocity : Vector2 = Vector2.ZERO
 var _speed : int = 150
+var hit := false
+
 
 func _physics_process(_delta):
 	var input := Vector2.ZERO
@@ -11,3 +13,12 @@ func _physics_process(_delta):
 	_velocity.y = input.y * _speed
 	
 	_velocity = move_and_slide(_velocity, Vector2.UP)
+	
+	
+	for i in get_slide_count():
+		var collision := get_slide_collision(i)
+		if collision.collider.is_in_group("Bullets") and hit == false:
+			hit = true
+			print("1")
+		if collision.collider.is_in_group("Wall"):
+			_speed = 100
