9e938d73c6c05547b1a4a3559290b77ab7ca48c0
Daniel <djisenberg2015@gmail.com>
2022-12-08 14:46:41 -0500

    Tweaked throwing star behavior and added some helpful comments

--- project/Scenes/Bullets/ThrowingStar.gd
+++ project/Scenes/Bullets/ThrowingStar.gd
@@ -1,21 +1,8 @@
 extends Area2D
 
-var origin
-var targetAngle
-var speed = 1
-	
-
-func _ready():
-#	var origin = get_parent().waveOrigin[2]
-#	origin.x = origin.x + 96  # this accounts for the offset of the arena, its actual origin is in the left wall, this puts it in the center.
-#
-#	var _randomAngle = rand_range(0,360)
-#	var _randomDistance = rand_range(150, 280)
-#
-#	self.position.x = origin.x + _randomDistance * cos(deg2rad(_randomAngle))
-#	self.position.y = origin.y + _randomDistance * sin(deg2rad(_randomAngle))
-	pass
-
+var origin  # This is passed in from the wave that generates this bullet.
+var targetAngle  # This is passed in from the wave that generates this bullet.
+var speed = 1.8
 
 
 func _process(delta):
--- project/Scenes/Waves/WaveShruiken.gd
+++ project/Scenes/Waves/WaveShruiken.gd
@@ -13,9 +13,10 @@ func _process(delta):
 	_spawnTimer = _spawnTimer + delta
 	if _spawnTimer >= _spawnInterval:
 		_spawnTimer = 0
-		var target = Vector2(rand_range(-96, 96), rand_range(-96, 96))
-		var spread = 30
-		var _randomAngle = rand_range(0,360)
+		
+		var target = Vector2(rand_range(-45, 45), rand_range(-45, 45))  # The range of allowed distance from the center of the arena for the bullets to target
+		var spread = 30  # The amount of spread, in degrees, between the 3 throwing stars.
+		var _randomAngle = rand_range(0,360)  # this line and the next provide the spawn point of the 3 shurikans, given in polar coordinates.
 		var _randomDistance = rand_range(150, 280)
 		
 		for i in range (0, 3):
