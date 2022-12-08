6f9dbfbfb70b60ffa2cd5c51608da1543407bbd3
Daniel <djisenberg2015@gmail.com>
2022-12-08 14:37:59 -0500

    Make throwing stars spawn in groups of 3 and aim at some point inside the arena, gradually spreading out in the process

--- project/Scenes/Bullets/ThrowingStar.gd
+++ project/Scenes/Bullets/ThrowingStar.gd
@@ -1,23 +1,27 @@
 extends Area2D
 
-var origin = Vector2(0,0)
-
+var origin
+var targetAngle
+var speed = 1
+	
 
 func _ready():
-	var origin = get_parent().waveOrigin[2]
-	origin.x = origin.x + 96  # this accounts for the offset of the arena, its actual origin is in the left wall, this puts it in the center.
-	
-	var _randomAngle = rand_range(0,360)
-	var _randomDistance = rand_range(150, 280)
-	
-	self.position.x = origin.x + _randomDistance * cos(deg2rad(_randomAngle))
-	self.position.y = origin.y + _randomDistance * sin(deg2rad(_randomAngle))
+#	var origin = get_parent().waveOrigin[2]
+#	origin.x = origin.x + 96  # this accounts for the offset of the arena, its actual origin is in the left wall, this puts it in the center.
+#
+#	var _randomAngle = rand_range(0,360)
+#	var _randomDistance = rand_range(150, 280)
+#
+#	self.position.x = origin.x + _randomDistance * cos(deg2rad(_randomAngle))
+#	self.position.y = origin.y + _randomDistance * sin(deg2rad(_randomAngle))
+	pass
 
 
 
 func _process(delta):
 	rotation_degrees = rotation_degrees + (-delta * 360)
-
+	self.position.x = self.position.x + speed * cos(targetAngle)
+	self.position.y = self.position.y + speed * sin(targetAngle)
 
 func bulletRemovalTrigger():
 	self.queue_free()
--- project/Scenes/Waves/WaveShruiken.gd
+++ project/Scenes/Waves/WaveShruiken.gd
@@ -1,7 +1,7 @@
 extends Node2D
 const bulletFactory = preload("res://Scenes/Bullets/ThrowingStar.tscn")
 
-const _spawnInterval = 0.005
+const _spawnInterval = 1
 var _spawnTimer = 0
 var waveOrigin
 
@@ -13,5 +13,22 @@ func _process(delta):
 	_spawnTimer = _spawnTimer + delta
 	if _spawnTimer >= _spawnInterval:
 		_spawnTimer = 0
-		var bullet = bulletFactory.instance()
-		self.add_child(bullet)
+		var target = Vector2(rand_range(-96, 96), rand_range(-96, 96))
+		var spread = 30
+		var _randomAngle = rand_range(0,360)
+		var _randomDistance = rand_range(150, 280)
+		
+		for i in range (0, 3):
+			var bullet = bulletFactory.instance()
+		
+			bullet.origin = waveOrigin[2]
+			bullet.origin.x = bullet.origin.x + 96
+			
+			bullet.position.x = bullet.origin.x + _randomDistance * cos(deg2rad(_randomAngle))
+			bullet.position.y = bullet.origin.y + _randomDistance * sin(deg2rad(_randomAngle))
+			
+			var targetAngle = bullet.get_angle_to(bullet.origin + target)
+			targetAngle = targetAngle - deg2rad(spread/3)
+			bullet.targetAngle = targetAngle + (i * deg2rad(spread/3))
+		
+			self.add_child(bullet)
