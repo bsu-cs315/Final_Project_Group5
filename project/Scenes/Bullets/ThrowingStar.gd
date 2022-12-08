adb6dd9d2a5bf100feb7ec58f186c82039b2c6d9
Daniel <djisenberg2015@gmail.com>
2022-12-08 14:11:30 -0500

    Make throwing stars spawn evenly only outside of arena

--- project/Scenes/Bullets/ThrowingStar.gd
+++ project/Scenes/Bullets/ThrowingStar.gd
@@ -1,8 +1,18 @@
 extends Area2D
 
+var origin = Vector2(0,0)
+
+
 func _ready():
-	self.position.x = rand_range(250, 450)
-	self.position.y = rand_range(200, 300)
+	var origin = get_parent().waveOrigin[2]
+	origin.x = origin.x + 96  # this accounts for the offset of the arena, its actual origin is in the left wall, this puts it in the center.
+	
+	var _randomAngle = rand_range(0,360)
+	var _randomDistance = rand_range(150, 280)
+	
+	self.position.x = origin.x + _randomDistance * cos(deg2rad(_randomAngle))
+	self.position.y = origin.y + _randomDistance * sin(deg2rad(_randomAngle))
+
 
 
 func _process(delta):
--- project/Scenes/Waves/WaveShruiken.gd
+++ project/Scenes/Waves/WaveShruiken.gd
@@ -1,8 +1,12 @@
 extends Node2D
 const bulletFactory = preload("res://Scenes/Bullets/ThrowingStar.tscn")
 
-const _spawnInterval = 1
+const _spawnInterval = 0.005
 var _spawnTimer = 0
+var waveOrigin
+
+func _ready():
+	waveOrigin = get_parent().get_transform()  # this gets the location of the center of the arena.
 
 
 func _process(delta):
